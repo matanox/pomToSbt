eec9aadab2c84ada9440fab7430d8dc99597e602
matan <dev.matan@gmail.com>
2015-03-02 19:08:04 +0200

    "initial commit"

---
+++ toSbt.scala
@@ -0,0 +1,23 @@
+import scala.xml._
+
+object pomToSbt {
+    def main(args: Array[String]) {
+      print("libraryDependencies ++= Seq(")
+      (XML.load("pomRefined.xml") \\ "dependencies") \ "dependency" foreach ((dependency: Node) => {
+        val groupId = (dependency \ "groupId").text
+        val artifactId = (dependency \ "artifactId").text
+        val version = (dependency \ "version").text
+        val scope = (dependency \ "scope").text
+        val classifier = (dependency \ "classifier").text
+        val artifactValName: String = artifactId.replaceAll("[-\\.]", "_")
+
+        print("\"%s\" %% \"%s\" %% \"%s\"".format(groupId, artifactId, version))
+        scope match {
+          case "" => print(",\n")
+          case _ => print(" %% \"%s\"\n".format(scope))
+        }
+        None
+      });
+    print(")\n")
+    }
+  }
